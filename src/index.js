00ff1bd7796512097f6a7fdc7c702262c490180d
Daniel Bamidele <greatbolutife@gmail.com>
2019-06-05 17:28:15 +0100

    setup project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './scss/app.scss';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
